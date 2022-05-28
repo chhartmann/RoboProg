d5570ceeec6c8a6bddcc4d8da6c73e05dc73aee8
chhartmann <chhartmann@web.de>
2022-05-28 18:10:35 +0000

    minor cleanup for ethernet

--- src/qemu_eth.cpp
+++ src/qemu_eth.cpp
@@ -11,34 +11,6 @@ static esp_eth_mac_t *s_mac = NULL;
 static esp_eth_phy_t *s_phy = NULL;
 static esp_eth_netif_glue_handle_t s_eth_glue = NULL;
 
-static void eth_event_handler(void *arg, esp_event_base_t event_base,
-                              int32_t event_id, void *event_data)
-{
-    uint8_t mac_addr[6] = {0};
-    /* we can get the ethernet driver handle from event data */
-    esp_eth_handle_t eth_handle = *(esp_eth_handle_t *)event_data;
-
-    switch (event_id) {
-    case ETHERNET_EVENT_CONNECTED:
-        esp_eth_ioctl(eth_handle, ETH_CMD_G_MAC_ADDR, mac_addr);
-        ESP_LOGI(TAG, "Ethernet Link Up");
-        ESP_LOGI(TAG, "Ethernet HW Addr %02x:%02x:%02x:%02x:%02x:%02x",
-                 mac_addr[0], mac_addr[1], mac_addr[2], mac_addr[3], mac_addr[4], mac_addr[5]);
-        break;
-    case ETHERNET_EVENT_DISCONNECTED:
-        ESP_LOGI(TAG, "Ethernet Link Down");
-        break;
-    case ETHERNET_EVENT_START:
-        ESP_LOGI(TAG, "Ethernet Started");
-        break;
-    case ETHERNET_EVENT_STOP:
-        ESP_LOGI(TAG, "Ethernet Stopped");
-        break;
-    default:
-        break;
-    }
-}
-
 static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
                                  int32_t event_id, void *event_data)
 {
@@ -46,16 +18,11 @@ static void got_ip_event_handler(void *arg, esp_event_base_t event_base,
     const esp_netif_ip_info_t *ip_info = &event->ip_info;
 
     ESP_LOGI(TAG, "Ethernet Got IP Address");
-    ESP_LOGI(TAG, "~~~~~~~~~~~");
     ESP_LOGI(TAG, "ETHIP:" IPSTR, IP2STR(&ip_info->ip));
-    ESP_LOGI(TAG, "ETHMASK:" IPSTR, IP2STR(&ip_info->netmask));
-    ESP_LOGI(TAG, "ETHGW:" IPSTR, IP2STR(&ip_info->gw));
-    ESP_LOGI(TAG, "~~~~~~~~~~~");
 }
 
 void eth_start(void) {
     ESP_ERROR_CHECK(esp_netif_init());
-    // Create default event loop that running in background
     ESP_ERROR_CHECK(esp_event_loop_create_default());
 
   char *desc;
@@ -63,10 +30,6 @@ void eth_start(void) {
   asprintf(&desc, "%s: %s", TAG, esp_netif_config.if_desc);
   esp_netif_config.if_desc = desc;
   esp_netif_config.route_prio = 64;
-//   esp_netif_config_t netif_config = {
-//       .base = &esp_netif_config,
-//       .stack = ESP_NETIF_NETSTACK_DEFAULT_ETH
-//   }; 
   esp_netif_config_t netif_config = ESP_NETIF_DEFAULT_ETH();
 
   esp_netif_t *netif = esp_netif_new(&netif_config);
@@ -75,8 +38,6 @@ void eth_start(void) {
 
   eth_mac_config_t mac_config = ETH_MAC_DEFAULT_CONFIG();
   eth_phy_config_t phy_config = ETH_PHY_DEFAULT_CONFIG();
-//  phy_config.phy_addr = 1;
-//  phy_config.reset_gpio_num = 5;
 
   // setup openeth for qemu
   phy_config.autonego_timeout_ms = 100;
@@ -95,7 +56,6 @@ void eth_start(void) {
 
     // Register user defined event handers
   ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_ETH_GOT_IP, &got_ip_event_handler, NULL));
-  // ESP_ERROR_CHECK(esp_event_handler_register(ETH_EVENT, ESP_EVENT_ANY_ID, &eth_event_handler, NULL));
 
     esp_eth_start(s_eth_handle);
 }
